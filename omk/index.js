e75442df4d367562e33c4779fb7941d0d53931da
YanHip <58882858+YanHip@users.noreply.github.com>
2021-11-28 19:38:13 +0100

    add data

--- omk/index.js
+++ omk/index.js
@@ -1,7 +1,7 @@
 var global_var = {
 	//513110937
 	//1340290303
-	url: "http://omeka-s/api/resource_templates",
+	url: "https://yanhip.github.io/omk/data/resource_templates.json",
 };
 main(global_var.url);
 
